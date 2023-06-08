2494b793165c51b02058b26dbf60e2c8d8198b42
JEISSON ALEXANDER GAVILAN MURCIA <jagavilanm@upn.edu.co>
2023-06-08 14:03:09 -0500

    Se agrega responsive a la sección proyectos

--- src/pages/Proyects.jsx
+++ src/pages/Proyects.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // Componets
 import LoadingPage from "../components/LoadingPage";
@@ -15,65 +15,70 @@ import "../styles/pages/projects.scss";
 import data from "../_data/projects.json";
 
 const Proyects = () => {
-    const [selectCategory, setSelectCategory] = useState(true);
-    const [itemSelect, setItemSelect] = useState("Todos");
+  const [selectCategory, setSelectCategory] = useState(true);
+  const [itemSelect, setItemSelect] = useState("Todos");
+  const [stateProjects, setStateProjects] = useState();
 
-    const Categories = data.categories.map((item, index) => {
-        return (
-            <CategoriesComponent
-                key={index}
-                id={item}
-                category={item}
-                componentClass={selectCategory && itemSelect === item ? "btn_active" : "btn_desactive"}
-                selectCategory={selectCategory}
-                setSelectCategory={setSelectCategory}
-                itemSelect={itemSelect}
-                setItemSelect={setItemSelect}
-            />
-        );
-    });
-
-    const Projects = data.projects.map((item, index) => {
-        return (
-            <Project
-                key={index}
-                img={item.img}
-                title={item.title}
-                category={item.category}
-                date={item.date}
-                text={item.text}
-                author={item.author}
-                location={item.location}
-                link={item.link}
-            />
-        );
-    });
-
+  const Categories = data.categories.map((item, index) => {
     return (
-        <>
-            <LoadingPage />
-            <Header />
-            <main className="main_projects">
-                <section className="title">
-                    <h1>{data.title}</h1>
-                    <p>
-                        {data.text1} <span>{data.textSpan}</span> {data.text2}
-                    </p>
-                </section>
-
-                <section className="container">
-                    <article className="filtrer">
-                        <h2>{data.categoryText}</h2>
-                        <ul>{Categories}</ul>
-                    </article>
-
-                    <article className="projects_container">{Projects}</article>
-                </section>
-            </main>
-            <Game />
-            <Footer />
-        </>
+      <CategoriesComponent
+        key={index}
+        id={item}
+        category={item}
+        componentClass={selectCategory && itemSelect === item ? "btn_active" : "btn_desactive"}
+        selectCategory={selectCategory}
+        setSelectCategory={setSelectCategory}
+        itemSelect={itemSelect}
+        setItemSelect={setItemSelect}
+      />
     );
+  });
+
+  const Projects = data.projects.map((item, index) => {
+    return (
+      <Project
+        key={index}
+        img={item.img}
+        title={item.title}
+        category={item.category}
+        date={item.date}
+        text={item.text}
+        author={item.author}
+        location={item.location}
+        link={item.link}
+      />
+    );
+  });
+
+  useEffect(() => {
+    setStateProjects(Projects);
+  }, []);
+
+  return (
+    <>
+      <LoadingPage />
+      <Header />
+      <main className="main_projects">
+        <section className="title">
+          <h1>{data.title}</h1>
+          <p>
+            {data.text1} <span>{data.textSpan}</span> {data.text2}
+          </p>
+        </section>
+
+        <section className="container">
+          <article className="filtrer">
+            <h2>{data.categoryText}</h2>
+            <ul>{Categories}</ul>
+          </article>
+
+          <article className="projects_container">{stateProjects}</article>
+        </section>
+      </main>
+      <Game />
+      <Footer />
+    </>
+  );
 };
 
 export default Proyects;
